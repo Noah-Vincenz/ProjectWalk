8b224a6bde72796fa6c7f1a5e410de98b78ee4d8
Alexandru Clapa <ion-alexandru.clapa@kcl.ac.uk>
2016-12-11 16:13:28 +0000

    Unused stuff

--- src/model/Networking.java
+++ src/model/Networking.java
@@ -4,12 +4,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import javax.xml.crypto.Data;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.*;
